8dcff9866a95e2b6b8d459a2cae0f15923dc1f63
Arne Meeuw <arne@exnaton.com>
2025-02-09 21:41:13 +0100

    Some calculation is running - not sure what the result should be..

--- ulp/ulp2berry/examples/lp_uart_print/lp_uart_print.be
+++ ulp/ulp2berry/examples/lp_uart_print/lp_uart_print.be
@@ -1,7 +1,7 @@
 var lp_uart_print = module('lp_uart_print')
 
 class lp_uart_print_class : Driver
-    var ulp_sleep_time, ulp_iteration, ulp_print_variable
+    var ulp_sleep_time, ulp_iteration, ulp_sml_state, ulp_print_variable, ulp_sml_byte, ulp_sml_t1wh
     
     def get_code()
       return bytes().fromb64("{{code_b64}}")
@@ -27,9 +27,24 @@ class lp_uart_print_class : Driver
       return ULP.get_mem({{ulp_iteration}})
     end
 
+    def read_state()
+      import ULP
+      return ULP.get_mem({{ulp_sml_state}})
+    end
+
     def read_print_variable()
       import ULP
       return ULP.get_mem({{ulp_print_variable}})
+    end
+
+    def read_byte()
+      import ULP
+      return ULP.get_mem({{ulp_sml_byte}})
+    end
+
+    def read_t1wh()
+      import ULP
+      return ULP.get_mem({{ulp_sml_t1wh}})
     end
 
     def set_print_variable(value)
@@ -38,10 +53,19 @@ class lp_uart_print_class : Driver
       return ULP.get_mem({{ulp_print_variable}})
     end
 
+    def set_byte(value)
+      import ULP
+      ULP.set_mem({{ulp_sml_byte}},value)
+      return ULP.get_mem({{ulp_sml_byte}})
+    end
+
     #- trigger a read every second -#
     def every_second()
       self.ulp_iteration = self.read_iteration()
       self.ulp_print_variable = self.read_print_variable()
+      self.ulp_sml_state = self.read_state()
+      self.ulp_sml_byte = self.read_byte()
+      self.ulp_sml_t1wh = self.read_t1wh()
     end
   
     #- display sensor value in the web UI -#
@@ -51,8 +75,14 @@ class lp_uart_print_class : Driver
                "{s}<hr>{m}<hr>{e}"
                "{s}ULP Variable{m}value:{e}"
                "{s}iteration{m}%i{e}"..
+               "{s}state{m}%i{e}"..
+               "{s}byte{m}%i{e}"..
+               "{s}t1wh{m}%f{e}"..
                "{s}print_variable{m}%i{e}",
                self.ulp_iteration,
+               self.ulp_sml_state,
+               self.ulp_sml_byte,
+               self.ulp_sml_t1wh,
                self.ulp_print_variable)
       tasmota.web_send_decimal(msg)
     end
@@ -81,6 +111,17 @@ if tasmota
     tasmota.resp_cmnd(string.format('{"print variable":%i}', result))
   end
   tasmota.add_cmd('lp_uart_print_variable', set_print_variable)
+
+  def set_byte(cmd, idx, payload, payload_json)
+    import ULP
+    import string
+    var result
+    if payload != ""
+      result = lp_uart_print.lp_uart_print.set_byte(int(payload))
+    end
+    tasmota.resp_cmnd(string.format('{"byte":%i}', result))
+  end
+  tasmota.add_cmd('lp_uart_byte', set_byte)
 end
 
 return lp_uart_print
--- ulp/ulp2berry/examples/lp_uart_print/main/lp_core/main.c
+++ ulp/ulp2berry/examples/lp_uart_print/main/lp_core/main.c
@@ -7,135 +7,73 @@
 #include "ulp_lp_core_print.h"
 #include "ulp_lp_core_utils.h"
 #include "ulp_lp_core_uart.h"
-#include "ehz_bin.h"
 #include "sml.h"
+#include "ehz_bin.h"
 
+sml_states_t sml_state;
 uint32_t iteration = 0;
 uint32_t print_variable = 1337;
+// long long int value = -1337;
+unsigned char sml_byte = 0xa7;
+const unsigned char obis[6] = {0x01, 0x00, 0x01, 0x08, 0x01, 0xff};
+double sml_t1wh = -2;
 
-double T1Wh = -2, SumWh = -2;
-
-// typedef struct
-// {
-//     const unsigned char OBIS[6];
-//     // void (*Handler)();
-// } OBISHandler;
-
-// void PowerT1() { smlOBISWh(&T1Wh); }
-
-// void PowerSum() { smlOBISWh(&SumWh); }
-
-// OBISHandler OBISHandlers[] = {
-//     {
-//         {0x01, 0x00, 0x01, 0x08, 0x01, 0xff}
-//         // , &PowerT1
-//     }, /*   1-  0:  1.  8.1*255 (T1) */
-//     {
-//         {0x01, 0x00, 0x01, 0x08, 0x00, 0xff},
-//         // , &PowerSum
-//     }, /*   1-  0:  1.  8.0*255 (T1 + T2) */
-//     {{
-//         0
-//         // , 0
-//     }}};
+#define LP_UART_PORT_NUM LP_UART_NUM_0
 
 int main(void)
 {
 
-    unsigned int i = 0;
-    sml_states_t s;
+    // uint8_t sml_byte = 0x1b;
+    // setState(SML_START, 4);
 
-    setState(SML_START, 4);
-    const unsigned char obis[6] = {0x01, 0x00, 0x01, 0x08, 0x01, 0xff};
+    iteration++;
+    print_variable++;
 
-    for (i = 0; i < ehz_bin_len; ++i)
+    // while (sml_byte != 0xa7 && sml_byte != 0xff)
+    /* Read data from the LP_UART */
+    // while (lp_core_uart_read_bytes(LP_UART_PORT_NUM, &sml_byte, 1, 10) == 1)
+    for (uint16_t i = 0; i < ehz_bin_len; i++)
     {
-        s = smlState(ehz_bin[i]);
-        // lp_core_printf("c: %d\n", c);
-        if (s == SML_START)
+        sml_byte = ehz_bin[i];
+        sml_state = smlState(sml_byte);
+        // lp_core_printf("sml_byte: %d\n", sml_byte);
+        if (sml_state == SML_START)
         {
-            lp_core_printf("SML_START\n");
+            // lp_core_printf("SML_START\n");
             /* reset local vars */
-            T1Wh = -3;
-            SumWh = -3;
+            sml_t1wh = -3;
         }
-        if (s == SML_LISTEND)
+        if (sml_state == SML_LISTEND)
         {
-            lp_core_printf("SML_LISTEND\n");
-
-            // for (i = 0; i < 6; i++)
-            // {
-            //     lp_core_printf("%d ", obis[i]);
-            // }
-            // lp_core_printf("\n");
-
-            // smlOBISCheck(obis);
+            // lp_core_printf("SML_LISTEND\n");
 
             const bool isMatch = smlOBISCheck(obis);
-            lp_core_printf("OBIS check\n");
+            // lp_core_printf("OBIS check\n");
             if (isMatch)
             {
-                lp_core_printf("Match - Processing value\n");
+                // lp_core_printf("Match - Processing value\n");
                 signed char scaler;
                 long long int value = smlOBISByUnit(&scaler, SML_WATT_HOUR);
                 if (value != -1)
                 {
-                    T1Wh = smlPow(value, scaler);
+                    // lp_core_printf("Value: %lld\n", value);
+                    sml_t1wh = smlPow(value, scaler);
                 }
             }
-            // if (smlOBISCheck(obis))
-            // {
-            //     lp_core_printf("OBIS found\n");
-            // }
-            // else
-            // {
-            //     lp_core_printf("OBIS not found\n");
-            // }
-
-            // int32_t val = 0;
-            // int8_t scaler = 0;
-
-            // val = smlOBISByUnit(&scaler, SML_WATT_HOUR);
-            // lp_core_printf("val: %d\n", val);
-            // T1Wh = smlPow(val, scaler);
-            // lp_core_printf("T1Wh: %f\n", T1Wh);
-
-            /* check handlers on last received list */
-            // for (iHandler = 0; OBISHandlers[iHandler].Handler != 0 &&
-            //                    !(smlOBISCheck(OBISHandlers[iHandler].OBIS));
-            //      iHandler++)
-            //     ;
-            // if (OBISHandlers[iHandler].Handler != 0)
-            // {
-            //     OBISHandlers[iHandler].Handler();
-            // }
         }
-        if (s == SML_UNEXPECTED)
+        if (sml_state == SML_UNEXPECTED)
         {
-            // lp_core_printf("i: %d\n", i);
-            lp_core_printf(">>> Unexpected byte! %d <<<\n", ehz_bin[i]);
+            // lp_core_printf(">>> Unexpected byte! %d <<<\n", sml_byte);
         }
-        if (s == SML_FINAL)
+        if (sml_state == SML_FINAL)
         {
-            lp_core_printf("SML_FINAL\n");
-            lp_core_printf("Power T1    (1-0:1.8.1)..: ");
-            lp_core_printf("%f", T1Wh);
-            lp_core_printf("\n");
-
-            //     lp_core_printf("Power T1+T2 (1-0:1.8.0)..: ");
-            //     lp_core_printf("%f", SumWh);
-            //     lp_core_printf("\n\n\n\n");
+            // lp_core_printf("SML_FINAL\n");
+            // lp_core_printf("Power T1    (1-0:1.8.1)..: ");
+            // lp_core_printf("%f", sml_t1wh);
+            // lp_core_printf("\n");
         }
+        sml_byte = -1;
     }
-
-    const char separator[] = "**************************";
-
-    lp_core_printf("Hello from the LP core!!\r\n");
-    lp_core_printf("This program has run %d times\r\n", ++iteration);
-    lp_core_printf("print_variable: %d\r\n", print_variable);
-    lp_core_printf("%s", separator);
-    lp_core_printf("\n");
-    lp_core_uart_tx_flush(LP_UART_NUM_0);
 
     return 0;
 }
