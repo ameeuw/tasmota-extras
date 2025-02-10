f7b22ef8717b3ddfc490dded33022a4bf06bead6
Arne Meeuw <arne@exnaton.com>
2025-02-10 20:48:30 +0100

    Use noop for variable persistence

--- ulp/ulp2berry/examples/lp_vars/main/lp_core/main.c
+++ ulp/ulp2berry/examples/lp_vars/main/lp_core/main.c
@@ -12,16 +12,15 @@ volatile float float_var = 1.23456789;
 volatile int32_t int_var = -123456789;
 volatile uint32_t uint_var = 123456789;
 volatile bool bool_var = true;
-volatile char string_var[] = "Hello, World! This could probably be longer. \nDepending on the amount of ram we are planning to eat up here.";
+volatile char string_var[] = "Hello, World! This could probably be longer. Depending on the amount of RAM we are planning to eat up here.";
 
 int main(void)
 {
     iteration++;
-
-    float float_var_local = float_var;
-    int int_var_local = int_var;
-    unsigned int uint_var_local = uint_var;
-    bool bool_var_local = bool_var;
-    char string_var_local = string_var[0];
+    (void)float_var;
+    (void)int_var;
+    (void)uint_var;
+    (void)bool_var;
+    (void)string_var;
     return 0;
 }
